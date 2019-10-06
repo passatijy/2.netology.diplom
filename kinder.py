350c549c8b0ed45e3d2edfc639a7a160f8dd9f58
passatijy <lmar@mail.ru>
2019-10-07 02:33:17 +0300

    advanced options

--- kinder.py
+++ kinder.py
@@ -3,6 +3,27 @@ from pymongo import MongoClient
 from datamongo import mk_mongodb, clear_db, find_by_name
 import time
 
+# > docker run -d -p 27017:27017 --name mongodb -v C:\Users\test\Projects\netology\2.Diplom\vkmongodb mongo
+# 
+def set_environment():
+    client = MongoClient('localhost')
+    dbnames = client.list_database_names()
+    if 'users_db' in dbnames:
+        work_db = client.users_db
+        print('Database users_db found, checking collection users_col')
+        if 'users_col' in client.users_db.collection_names():
+            print('Collection users_col exist, ')
+        else:
+            print('Collection users_col not exist, creating')
+            work_db.users_col
+            work_db.skip_number
+    else:
+        print('db not found, creating')
+        work_db = client.users_db
+        work_db.users_col
+        work_db.skip_number
+    return work_db
+
 
 def univers_req(login, password):
     vk_session = vk_api.VkApi(login, password)
@@ -42,13 +63,12 @@ def user_search(login, password, **search_param):
     resp = univers_req(login, password).users.search(**search_param)
     return resp
 
-def user_search_cities(login, password, city, sex, age_to):
+def user_search_cities(login, password, city, sex, age_to, count, offset):
     for k in (city):
         search_result = []
-        offset = input('Input offset: ')
         search_result = user_search(login, password,
             city=k, 
-            count=80,
+            count=count,
             sex=sex, 
             status='6', 
             age_from = '19',
@@ -72,7 +92,7 @@ def users_get(login, password, user_id):
         result.append(groups)
         result.append(friends)
     else:
-        print('User with id', user_id,'and name', resp[0]['first_name'], resp[0]['last_name'], 'have closed profile.')
+        print('User with id', user_id, 'have closed profile.')
     return result
 
 def filling_base(db, collection, user_list):
@@ -80,28 +100,39 @@ def filling_base(db, collection, user_list):
     for k in user_list:
         for n in k['items']:
             tmp_user = users_get(login, password, n['id'])
-            tmp_user[0][0]['groups'] = tmp_user[1]
-            tmp_user[0][0]['friends'] = tmp_user[2]
-            db[collection].insert(tmp_user[0][0])
-            print('user detail was get succesfully ')
+            if len(tmp_user) > 0:
+                tmp_user[0][0]['groups'] = tmp_user[1]
+                tmp_user[0][0]['friends'] = tmp_user[2]
+                db[collection].insert(tmp_user[0][0])
+                print('user detail was get succesfully ')
+            else:
+                print('Looks like this user closed his profile.')
 
 def find_like_value(target_user_id, database, collection, match_field, field_weight):
     my_user_db = database[collection].find_one({'id': target_user_id})
     for user_doc in database[collection].find():
         #print(user_doc[match_field])
-        counter = 0
-        print('looking in user:', user_doc['first_name'],user_doc['last_name'])
-        #print('target user ',match_field,' list:', my_user_db[match_field])
-        for one_group in user_doc[match_field]['items']:
-            # print('one_group', one_group)
-            if one_group in my_user_db[match_field]['items']:
-                counter +=1
-                print('user group matched with target, counter: ', counter)
-        if 'like_value' in user_doc.keys():
-            like_calcul = user_doc['like_value'] + counter*field_weight
+        iscounted = match_field + '_iscounted'
+        if iscounted in user_doc:
+            print('this user', user_doc['first_name'],
+                user_doc['last_name'], 
+                'is allways counted by field', match_field)
         else:
-            like_calcul = counter*field_weight
-        database.users_col.update_one({'id': user_doc['id']}, {'$set': {'like_value': like_calcul}})
+                print('not counted, proceed evaluation')
+                counter = 0
+                print('looking in user:', user_doc['first_name'],user_doc['last_name'])
+                #print('target user ',match_field,' list:', my_user_db[match_field])
+                for one_group in user_doc[match_field]['items']:
+                    # print('one_group', one_group)
+                    if one_group in my_user_db[match_field]['items']:
+                        counter +=1
+                        print('user group matched with target, counter: ', counter)
+                if 'like_value' in user_doc.keys():
+                    like_calcul = user_doc['like_value'] + counter*field_weight
+                else:
+                    like_calcul = counter*field_weight
+                database.users_col.update_one({'id': user_doc['id']}, {'$set': {'like_value': like_calcul}})
+                database.users_col.update_one({'id': user_doc['id']}, {'$set': {iscounted: True}})
 
 def show_coll(db, collection):
     coll_size = 0
@@ -113,37 +144,53 @@ def show_coll(db, collection):
             print(k['id'],k['first_name'],k['last_name'])
     print('Collection size:', coll_size)
 
+def update_offset(work_db):
+    count = input('Сколько пользователей запросить из VK: ')
+    if work_db.skip_number.count_documents({}):
+        oldoffset = work_db.skip_number.find({},{'skip':1, '_id':0})[0]['skip']
+        newoffset = oldoffset + int(count)
+        work_db.skip_number.find_and_modify(query={'what':'thisiscounter'}, 
+            update={"$set": {'skip':newoffset}})
+        print('Новое смещение от начала списка пользователей:',
+            work_db.skip_number.find_one({'what':'thisiscounter'})['skip'])
+    else:
+        print('cant find offset number, inserting')
+        newoffset = count
+        oldoffset = 0
+        work_db.skip_number.insert({'skip':int(count), 'what':'thisiscounter'})
+    return oldoffset, count, newoffset
 
+
+def clear_db(dbname, collname):
+    client = MongoClient('localhost')
+    db = client[dbname]
+    mycol = db[collname]
+    mycol.drop()
 
 login = 'NetologyPythonVk@yandex.ru'
-
+password = ''
 #my id: 552934290
 
 if __name__ == '__main__':
 # wall_get(login, password)
-    client = MongoClient('localhost')
-    dbnames = client.list_database_names()
-    if 'users_db' in dbnames:
-        work_db = client.users_db
-        print('Database users_db found, checking collection users_col')
-        if 'users_col' in client.users_db.collection_names():
-            print('Collection users_col exist, ')
-        else:
-            print('Collection users_col not exist, creating')
-            work_db.users_col
-    else:
-        print('db not found, creating')
-        work_db = client.users_db
-        work_db.users_col
+    work_db = set_environment()
+
+    count = update_offset(work_db)
+
 
     long_user_list = []
-    long_user_list = user_search_cities(login, password, '1', '1', '35' )
+    newoffset = work_db.skip_number.find_one({'what':'thisiscounter'})['skip']
+    long_user_list = user_search_cities(login, password, '1', '1', '35',count[1], count[0])
 
 # insert my own user in database:
     my_user = users_get(login, password, '552934290')
     my_user[0][0]['groups']=my_user[1]
     my_user[0][0]['friends']=my_user[2]
-    work_db.users_col.insert(my_user[0][0])
+    if work_db.users_col.find_one({'id':my_user[0][0]['id']}):
+        print('My own user exist, not inserting')
+    else:
+        print('My own user not exist, adding in database')
+        work_db.users_col.insert(my_user[0][0])
 
     filling_base(work_db, 'users_col', long_user_list)
 
